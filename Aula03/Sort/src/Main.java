9217960309ce2c90bd949951410b7c08bbe718cd
Vinicius de Ávila Bezerra <vinibezerra2004@gmail.com>
2026-08-17 20:55:18 -0300

    add Insertion Sort

--- Aula03/Sort/src/Main.java
+++ Aula03/Sort/src/Main.java
@@ -1,9 +1,13 @@
 import ordenacao.BubbleSort;
 import ordenacao.InsertSort;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         BubbleSort.sort(new int[]{2, 3, 10, 1});
-        InsertSort.sort(new int[]{2, 3, 10, 1});
+        System.out.println("---- Insertion ----");
+        int[] sorted = InsertSort.sort(new int[]{2, 3, 10, 1});
+        System.out.println(Arrays.toString(sorted));
     }
 }
--- Aula03/Sort/src/ordenacao/InsertSort.java
+++ Aula03/Sort/src/ordenacao/InsertSort.java
@@ -2,6 +2,16 @@ package ordenacao;
 
 public class InsertSort {
     public static int[] sort(int[] arr){
-        return null;
+        int n = arr.length;
+        for(int i = 1; i< n; i++){
+            int key = arr[i];
+            int j = i -1;
+            while(j >= 0 && arr[j] > key ){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+        return arr;
     }
 }
