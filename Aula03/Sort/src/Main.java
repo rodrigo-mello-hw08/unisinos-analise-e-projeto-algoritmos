b5637bb1c9480cb1de436351af572307f2830441
rodrigo.mello <rodrigo.mello.hw08@gmail.com>
2026-08-17 20:48:31 -0300

    add algoritimos ordenacao

--- Aula03/Sort/src/Main.java
+++ Aula03/Sort/src/Main.java
@@ -1,15 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import ordenacao.BubbleSort;
+import ordenacao.InsertSort;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        BubbleSort.sort(new int[]{2, 3, 10, 1});
+        InsertSort.sort(new int[]{2, 3, 10, 1});
     }
 }
---
+++ Aula03/Sort/src/ordenacao/InsertSort.java
@@ -0,0 +1,7 @@
+package ordenacao;
+
+public class InsertSort {
+    public static int[] sort(int[] arr){
+        return null;
+    }
+}
